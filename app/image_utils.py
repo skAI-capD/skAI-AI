3a88a6a8e7e8a36fe4da580e23e34a9f9c600b24
정민영 <jmy0727@itc.ulsan.ac.kr>
2025-05-29 19:48:34 +0900

    plz

--- app/image_utils.py
+++ app/image_utils.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
-
+#걍 추가
 # 7. 이미지 생성
 def generate_diary_image(prompt: str) -> str:
     response = client.images.generate(
